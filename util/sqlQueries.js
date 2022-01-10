2aa0be40bc74b304eb6da7d8710576de7dd336fe
joshgit <jschermann91@gmail.com>
2022-01-09 18:43:42 -0600

    readme updated

--- util/sqlQueries.js
+++ util/sqlQueries.js
@@ -246,6 +246,7 @@ const addEmployee = async () => {
       },
     ]);
     await viewRoles();
+
     const roleQuestion = await inquirer.prompt([
       {
         type: "input",
@@ -524,7 +525,7 @@ const viewBudget = async () => {
 // Try to add some additional functionality to your application, such as
 // the ability to do the following:
 // Update employee managers.
-// View employees by manager,
+// View employees by manager, --
 // View employees by department.
 // Delete departments, roles, and employees. - - -
 // View the total utilized budget of a department—in other words, -------

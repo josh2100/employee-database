feef2b09df64502683a97986f736626505d743bd
joshgit <jschermann91@gmail.com>
2022-01-09 20:40:42 -0600

    schema clean up

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL, -- Reference to department role belongs to
+  department_id INTEGER NOT NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
@@ -19,46 +19,8 @@ CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR (30),
-  role_id INTEGER NOT NULL, -- Constraint with role?, connects to department, salary
+  role_id INTEGER NOT NULL,
   manager_id INTEGER,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
   CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
 );
-
--- DROP TABLE IF EXISTS votes;
--- DROP TABLE IF EXISTS candidates;
--- DROP TABLE IF EXISTS parties;
--- DROP TABLE IF EXISTS voters;
-
--- CREATE TABLE parties (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   name VARCHAR(50) NOT NULL,
---   description TEXT
--- );
-
--- CREATE TABLE candidates (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   party_id INTEGER,
---   industry_connected BOOLEAN NOT NULL,
---   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
--- );
-
--- CREATE TABLE voters (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
---   email VARCHAR(50) NOT NULL,
---   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
--- );
-
--- CREATE TABLE votes (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   voter_id INTEGER NOT NULL,
---   candidate_id INTEGER NOT NULL,
---   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
---   CONSTRAINT uc_voter UNIQUE (voter_id),
---   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
---   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
--- );
